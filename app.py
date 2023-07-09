8f202fbd97b0880d6f675cf40a3a038b2abf21db
heroding77 <heroding77@qq.com>
2023-07-09 10:36:20 +0800

    xxx

--- app.py
+++ app.py
@@ -13,50 +13,27 @@ def index():
 def home():
     return index()
 
-
-@app.route('/video')
-def video():
-    return render_template("video.html")
-
 @app.route('/map')
 def map():
     return render_template("map.html")
 
-@app.route('/line_map')
-def line_map():
-    return render_template("line_map.html")
 
-@app.route('/line_map2')
-def line_map2():
-    return render_template("line_map2.html")
+@app.route('/openrank')
+def map():
+    return render_template("openrank.html")
 
-@app.route('/line_map3')
-def line_map3():
-    return render_template("line_map3.html")
+@app.route('/activity')
+def map():
+    return render_template("activity.html")
 
-@app.route('/some_data')
-def some_data():
-    return render_template("some_data.html")
+@app.route('/attention')
+def map():
+    return render_template("attention.html")
 
-@app.route('/Projections1')
-def Projections1():
-    return render_template("Projections1.html")
+@app.route('/stars')
+def map():
+    return render_template("stars.html")
 
-@app.route('/Projections2')
-def Projections2():
-    return render_template("Projections2.html")
-
-@app.route('/Projections3')
-def Projections3():
-    return render_template("Projections3.html")
-
-@app.route('/Projections4')
-def Projections4():
-    return render_template("Projections4.html")
-
-@app.route('/Projections5')
-def Projections5():
-    return render_template("Projections5.html")
 
 @app.route('/favicon.ico')
 def favicon():
